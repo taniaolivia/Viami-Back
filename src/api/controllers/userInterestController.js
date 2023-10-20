218fbb57b22f2824d71e61f41a3c471e5c127362
Tania Olivia <tania.olivia69@yahoo.com>
2023-10-20 22:59:39 +0200

    add functions to get all users interests with their users and get all interests of a user by id

---
+++ src/api/controllers/userInterestController.js
@@ -0,0 +1,32 @@
+const db = require("../knex");
+
+// Get all interests with their users
+exports.getAllUsersInterests = (req, res) => {
+    db("user_interest")
+        .select("*")
+        .join("user", "user.id", "=", "user_interest.userId")
+        .join("interest", "interest.id", "=", "user_interest.interestId")
+        .then(data => res.status(200).json({data}))
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Server error"});
+        });
+}
+
+// Get all interests of a user by id
+exports.getUserInterestsById = (req, res) => {
+    let id = req.params.userId;
+
+    db("user_interest")
+        .select("*")
+        .where({userId: id})
+        .join("user", "user.id", "=", "user_interest.userId")
+        .join("interest", "interest.id", "=", "user_interest.interestId")
+        .then(data => res.status(200).json({data}))
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Server error"});
+        });
+}
