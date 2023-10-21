6ad998192923bbe3d280f2ec8fd2777c86a6a90c
Tania Olivia <tania.olivia69@yahoo.com>
2023-10-21 23:28:12 +0200

    add route and functions to get list of languages and a language

---
+++ src/api/controllers/languageController.js
@@ -0,0 +1,28 @@
+const db = require("../knex");
+
+// Get a list of languages
+exports.listAllLanguages = (req, res) => {
+    db("language")
+        .select("*")
+        .then(data => res.status(200).json({"languages": data}))
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Server error"});
+        });
+}
+
+// Get a language by id
+exports.getLanguageById = (req, res) => {
+    let id = req.params.languageId;
+
+    db("language")
+        .select("*")
+        .where({id: id})
+        .then(data => res.status(200).json({data}))
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Server error"});
+        });
+}
