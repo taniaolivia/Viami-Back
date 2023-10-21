d100507321fbedeca42baa288fe98e87fc38c141
Tania Olivia <tania.olivia69@yahoo.com>
2023-10-21 23:51:38 +0200

    add routes and functions to add, delete and get the user's images

---
+++ src/api/controllers/userImageController.js
@@ -0,0 +1,89 @@
+const db = require("../knex");
+
+// Get all images with their users
+exports.getAllUsersImages = (req, res) => {
+    db("user_image")
+        .select("*")
+        .join("user", "user.id", "=", "user_image.userId")
+        .join("image", "image.id", "=", "user_image.imageId")
+        .then(data => res.status(200).json({data}))
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Server error"});
+        });
+}
+
+// Get all images of a user by id
+exports.getUserImagesById = (req, res) => {
+    let id = req.params.userId;
+
+    db("user_image")
+        .select("*")
+        .where({userId: id})
+        .join("user", "user.id", "=", "user_image.userId")
+        .join("image", "image.id", "=", "user_image.imageId")
+        .then(data => res.status(200).json({"userImages": data}))
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Server error"});
+        });
+}
+
+// Add image to user's data
+exports.addUserImage = (req, res) => {
+    let image = req.body.imageId;
+    let user = req.params.userId;
+
+    db("user_image")
+        .insert({
+            imageId: image,
+            userId: user
+        })
+        .then(data => {
+            db("user")
+                .select("*")
+                .where({id: user})
+                .then(userData => {
+                    db("image")
+                        .select("*")
+                        .where({id: image})
+                        .then(imageData => {
+                            res.status(200).json({
+                                message: `Image is added to user's data`,
+                                user: userData,
+                                image: imageData
+                            })
+                    })
+                })
+        })
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Invalid request"});
+        })
+}
+
+// Delete an image in user's data
+exports.deleteUserImage = (req, res) => {
+    let image = req.body.imageId;
+    let user = req.params.userId;
+
+    db("user_image")
+        .delete("*")
+        .where({
+            imageId: image,
+            userId: user
+        })
+        .then(data => {
+            res.status(200).json({
+                message: `Image is deleted from user's data`,
+            });
+        })
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Invalid request"});
+        })
+}
