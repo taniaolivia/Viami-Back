09037206896aed2aa2cdb4448994f2e4c7c706f9
Tania Olivia <tania.olivia69@yahoo.com>
2023-10-30 01:17:25 +0100

    fix the user comments functions

--- src/api/controllers/userCommentController.js
+++ src/api/controllers/userCommentController.js
@@ -4,7 +4,7 @@ const db = require("../knex");
 exports.getAllUsersComments = (req, res) => {
     db("user_comment")
         .select("*")
-        .join("user", "user.id", "=", "user_comment.commentId")
+        .join("user", "user.id", "=", "user_comment.userId")
         .join("comment", "comment.id", "=", "user_comment.commentId")
         .then(data => res.status(200).json({data}))
         .catch(error => {
@@ -21,7 +21,7 @@ exports.getUserCommentsById = (req, res) => {
     db("user_comment")
         .select("*")
         .where({userId: id})
-        .join("user", "user.id", "=", "user_comment.commentId")
+        .join("user", "user.id", "=", "user_comment.userId")
         .join("comment", "comment.id", "=", "user_comment.commentId")
         .then(data => res.status(200).json({"userComments": data}))
         .catch(error => {
