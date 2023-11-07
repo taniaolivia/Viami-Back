849d030ffa07be8d384b0c1f5e5c19e2914ba2a1
nihelouanassi <nihelouanasi@gmail.com>
2023-11-07 15:24:57 +0100

    add some route to travel activity route

--- src/api/routes/travelActivityRoute.js
+++ src/api/routes/travelActivityRoute.js
@@ -7,5 +7,11 @@ server.route("/api/travelActivities")
 .get(jwtMiddleware.authenticateUser, cors(), travelActivityController.getAllTravelsActivities);
 
 
+server.route("/api/travels/:travelId/activities")
+.get(jwtMiddleware.authenticateUser, cors(), travelActivityController.getTravelActivitiesById)
+.post(jwtMiddleware.authenticateUser, cors(), travelActivityController.addActivityToTravel)
+.delete(jwtMiddleware.authenticateUser, cors(), travelActivityController.deleteTravelActivity);
+
+
 
 }
