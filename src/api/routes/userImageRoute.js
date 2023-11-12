f7b69cc5c05c7d3593b17c554269d6c12c0b3aba
Tania Olivia <tania.olivia69@yahoo.com>
2023-11-12 23:41:58 +0100

    get upload

--- src/api/routes/userImageRoute.js
+++ src/api/routes/userImageRoute.js
@@ -2,13 +2,17 @@ module.exports = (server) => {
     const userImageController = require("../controllers/userImageController");
     const jwtMiddleware = require("../middlewares/jwtMiddleware");
     const cors = require('cors');
+    const cors = require('cors');
+    const multer = require('multer');
+    const storage = multer.memoryStorage();
+    const upload = multer({ storage: storage });
 
 server.route("/api/usersImages")
 .get(jwtMiddleware.authenticateUser, cors(), userImageController.getAllUsersImages);
 
 server.route("/api/users/:userId/images")
 .get(jwtMiddleware.authenticateUser, cors(), userImageController.getUserImagesById)
-.post(jwtMiddleware.authenticateUser, cors(), userImageController.addUserImage)
+.post(jwtMiddleware.authenticateUser, upload.single('image'), cors(), userImageController.addUserImage)
 .delete(jwtMiddleware.authenticateUser, cors(), userImageController.deleteUserImage);
 
 }
