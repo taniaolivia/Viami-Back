75ca51222824e06c948c12d263c07074cef35b78
Tania Olivia <tania.olivia69@yahoo.com>
2023-11-12 07:38:02 +0100

    test upload image to aws s3

--- src/api/controllers/imageController.js
+++ src/api/controllers/imageController.js
@@ -1,4 +1,6 @@
 const db = require("../knex");
+const AWS = require('aws-sdk');
+const crypto = require('crypto');
 
 // Get a list of images
 exports.listAllImages = (req, res) => {
@@ -40,24 +42,51 @@ exports.addImage = (req, res) => {
                 res.json({message: "Image already exists"});
             }
             else {
-                    db("image")
-                    .insert({
-                        image: newImage.image
-                    })
-                    .then(data => {
-                        res.status(200).json({
-                            message: `Image added successfully`,
-                            data: {
-                                id: data[0],
-                                image: newImage.image
-                            }                    
-                        })
-                    })
-                    .catch(error => {
-                        res.status(401);
-                        console.log(error);
-                        res.json({message: "Invalid request"});
-                    });
+                AWS.config.update({
+                    accessKeyId: PROCESS_ENV_ACCESS_KEY,
+                    secretAccessKey: PROCESS_ENV_SECRET_KEY,
+                    region: PROCESS_ENV_REGION
+                });
+
+                const s3 = new AWS.S3();
+                const bucketName = PROCESS_ENV_BUCKET_NAME;
+                const uniqueId = Date.now();
+                const randomString = crypto.randomBytes(4).toString('hex');
+                
+                const key = `image_${uniqueId}_${randomString}.jpg`;
+            
+                const params = {
+                    Bucket: bucketName,
+                    Key: key,
+                    Body: fileContent,
+                    ContentType: 'image/jpeg' 
+                };
+
+                s3.upload(params, (err, data) => {
+                    if (err) {
+                        console.error("Error uploading image to S3:", err);
+                    } else {
+                        db("image")
+                            .insert({
+                                image: data.location
+                            })
+                            .then(data => {
+                                res.status(200).json({
+                                    message: `Image added successfully`,
+                                    data: {
+                                        id: data[0],
+                                        image: data.location
+                                    }                    
+                                })
+                            })
+                            .catch(error => {
+                                res.status(401);
+                                console.log(error);
+                                res.json({message: "Invalid request"});
+                            });
+                    }
+                });
+                    
             }
         })
 }
