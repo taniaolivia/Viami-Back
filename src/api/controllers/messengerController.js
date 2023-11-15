c1998d7fc7b3338a9387f458c43ba8b2b9c9ca2d
Tania Olivia <tania.olivia69@yahoo.com>
2023-11-15 12:29:11 +0100

    add functions to list all messagers of a user and between two users

---
+++ src/api/controllers/messengerController.js
@@ -0,0 +1,40 @@
+const db = require("../knex");
+
+// Get a list of all messages sent by a user
+exports.listAllMessagesBySenderId = (req, res) => {
+    let senderId = req.params.senderId;
+    
+    db("message")
+        .select("*")
+        .where("senderId", senderId)
+        .join("user", "user.id", "=", "message.senderId")
+        .join("user", "user.id", "=", "message.responderId")
+        .orderBy("date", "asc")
+        .then(data => res.status(200).json({"messages": data}))
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Server error"});
+        });
+}
+
+// Get a list of all messages sent between two users
+exports.listAllMessagesBySenderResponder = (req, res) => {
+    let senderId = req.params.senderId;
+    let responderId = req.params.responderId;
+
+    db("message")
+        .select("*")
+        .where("senderId", senderId)
+        .where("responderId", responderId)
+        .join("user", "user.id", "=", "message.senderId")
+        .join("user", "user.id", "=", "message.responderId")
+        .orderBy("date", "asc")
+        .then(data => res.status(200).json({"messages": data}))
+        .catch(error => {
+            res.status(401);
+            console.log(error);
+            res.json({message: "Server error"});
+        });
+}
+
