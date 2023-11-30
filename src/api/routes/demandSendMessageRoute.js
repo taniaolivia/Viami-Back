e9cea35ad3cf4f039f13395a66fe2f92f451536c
Tania Olivia <tania.olivia69@yahoo.com>
2023-11-30 23:59:19 +0100

    add routes and fonctions related to sending and answering request to send a message to a user

--- src/api/routes/demandSendMessageRoute.js
+++ src/api/routes/demandSendMessageRoute.js
@@ -3,8 +3,18 @@ module.exports = (server) => {
     const jwtMiddleware = require("../middlewares/jwtMiddleware");
     const cors = require('cors');
 
+server.route("/api/send/requestMessage")
+.post(jwtMiddleware.authenticateUser, cors(), demandSendMessageController.requestSendMessage);
 
-server.route("/api/demandSendMessage")
-.post(jwtMiddleware.authenticateUser, cors(), demandSendMessageController.demandSendMessage);
+server.route("/api/send/answerRequest")
+.patch(jwtMiddleware.authenticateUser, cors(), demandSendMessageController.answerRequestMessageTwoUsers);
 
+server.route("/api/requestsMessages/:receiverId")
+.get(jwtMiddleware.authenticateUser, cors(), demandSendMessageController.getAllRequestsMessagesByUser);
+
+server.route("/api/requestsMessages/:userId/accepted")
+.get(jwtMiddleware.authenticateUser, cors(), demandSendMessageController.getAllAcceptedRequestsByUser);
+
+server.route("/api/requestsMessages/:requestId/chat")
+.patch(jwtMiddleware.authenticateUser, cors(), demandSendMessageController.setChat);
 }
