7b36f2966e7a573f55c3099e66cda8ce1c78ff1b
Tania Olivia <tania.olivia69@yahoo.com>
2023-12-19 12:28:46 +0100

    add a function and route to get the nearest activities

--- src/api/controllers/activityController.js
+++ src/api/controllers/activityController.js
@@ -1,14 +1,15 @@
 const db = require("../knex");
+const EARTH_RADIUS = 6371; // Earth's radius in kilometers
 
 exports.listAllActivities = (req, res) => {
     db("activity")
-    .select("*")
-    .orderBy("name", "asc")
-    .then(data => res.status(200).json({"activities": data}))
-    .catch(error => {
-        res.status(401);
-        res.json({message: "Server error"});
-    });   
+        .select("*")
+        .orderBy("name", "asc")
+        .then(data => res.status(200).json({"activities": data}))
+        .catch(error => {
+            res.status(401);
+            res.json({message: "Server error"});
+        });   
 }
 
 exports.getActivityById = (req, res) => {
@@ -123,7 +124,7 @@ exports.getRecommendedActivityById = (req, res) => {
         });
 };
 
-//update note activity
+// Update note activity
 exports.updateNoteActivity=(req,res) => {
     const id = req.params.activityId;
     const newNote = req.body.note;
@@ -140,11 +141,54 @@ exports.updateNoteActivity=(req,res) => {
         console.log(error);
         res.json({message: "Activity not found"});
     });
-
-
-
 }
 
+// Function to calculate distance between two points using Haversine formula
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * (Math.PI / 180)) *
+            Math.cos(lat2 * (Math.PI / 180)) *
+            Math.sin(dLon / 2) *
+            Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = EARTH_RADIUS * c;
+
+    return distance;
+}
+
+// Get the nearest activity by user's position
+exports.getAllActivitiesByUserPosition = (req, res) => {
+    const userLat = req.query.lat;
+    const userLon = req.query.lon;
+
+    db("activity")
+        .select("*")
+        .then(activities => {
+            const nearbyActivities = activities.filter(activity => {
+                const distance = calculateDistance(
+                    userLat,
+                    userLon,
+                    activity.latitude,
+                    activity.longitude
+                );
+
+                return distance <= 50;
+            });
+
+            if (nearbyActivities.length > 0) {
+                res.status(200).json({ message: "Nearby activities found", activities: nearbyActivities });
+            } else {
+                res.status(404).json({ message: "No nearby activities found" });
+            }
+        })
+        .catch(error => {
+            res.status(404);
+            res.json({message: "Activity not found"});
+        });
+};
 
 
 
