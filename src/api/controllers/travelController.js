bcd6e99d616e2f3f2cac545bf71c89a8e2dfb8be
Tania Olivia <tania.olivia69@yahoo.com>
2024-04-16 22:47:57 +0200

    test

--- src/api/controllers/travelController.js
+++ src/api/controllers/travelController.js
@@ -41,16 +41,69 @@ exports.getDateLocationUsers = async (req, res) => {
 };
 
 // Add user to a trip that user has searched
-exports.joinTravel = async (req, res) => {
-    try {
-        const { location, date, userId } = req.body;
+exports.joinTravel = (req, res) => {
+    let location = req.body.location;
+    let date = req.body.date;
+    let userId = req.body.userId
 
-        const message = await dateLocationService.joinTravel(location, date, userId);
+    db("date_location")
+    .select("*")
+    .where("location", location)
+    .where("date", date)
+    .orderBy("date", "asc")
+    .then(data => {
+        console.log("data", data)
 
-        res.status(200).json({ message });
-    } catch (error) {
-        res.status(401).json({ message: error.message });
-    }
+        db("user_date_location")
+            .select("*")
+            .where("dateLocationId", data[0].id)
+            .andWhere("userId", userId)
+            .then(user => {
+                if(user.length === 0) {
+                    db("user_date_location")
+                    .insert({
+                        userId: userId,
+                        dateLocationId: data[0].id
+                    })
+                    .then(data => {
+                        db("date_location")
+                            .update({
+                                id: data[0].id,
+                                date: data[0].date,
+                                location: data[0].location,
+                                nbParticipant: data[0].nbParticipant == null ? 1 : data[0].nbParticipant + 1
+                            }).
+                            then(data => {
+                                res.status(200);
+                                res.json({message: "You've been added successfully to the trip!"});
+                            })
+                            .catch(error => {
+                                res.status(401);
+                                res.json({message: "Problem when adding user to a trip"});
+                            }); 
+                    })
+                    .catch(error => {
+                        res.status(401);
+                        res.json({message: "Failed to add user to the trip"});
+                    }); 
+                }
+                else {
+                    res.status(200);
+                    res.json({message: "You've joined this trip"});
+                }
+            
+            })
+            .catch(error => {
+                res.status(401);
+                res.json({message: "Failed to add user to the trip"});
+            }); 
+    
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500);
+        res.json({message: "Server error"});
+    }); 
 };
 
 // Get all travels
